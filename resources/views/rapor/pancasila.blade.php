0d1a73d9a8f7a7dd63dcf7fe252607ad0387cf70
Zidane <zidanesanjaya81@Gmail.com>
2023-08-22 20:33:14 +0700

    Update pancasila.blade.php

--- resources/views/rapor/pancasila.blade.php
+++ resources/views/rapor/pancasila.blade.php
@@ -7,168 +7,306 @@
   <title>Pancasila</title>
 
   <style>
-    .image-container {
-      position: relative;
-      width: 487.5pt;
-    }
-
-    .image-overlay {
+    .bgCover{
+      width: height;
+      height: 790%;
+      /* height: 700px; */
       position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(255, 255, 255, 0.5);
-      /* Warna latar belakang transparan */
+      background-size: cover;
+      background-blend-mode: overlay;
+      opacity: 0.1;
+      margin-top: 24%;
+      margin-left: 2%;
     }
   </style>
 </head>
 
 <body style="font-family: Arial, Helvetica, sans-serif;">
-  <div class="image-container">
-    <div class="image-overlay"></div>
-    <div style="border-style: solid; width: 487.5pt; background-image: url('/images/logohead.png'); background-repeat: no-repeat; background-size: cover; opacity: 1;">
-      <table style="border-collapse: collapse; margin-left: 70pt" cellspacing="0">
-        <tr style="height: 50pt;">
-          <td style="width: 70pt;">
-            <img src="/images/logohead.png" alt="" style="width: height; height: 65px;">
-          </td>
-          <td style="font-weight: bold; font-family: Arial, Helvetica, sans-serif; text-align: center;">
-            RAPOR PROJEK<br> PENGUATAN PROFIL PELAJAR PANCASILA
-          </td>
-          <td style="width: 65pt; text-align: end;">
-            <img src="/images/logohead.png" alt="" style="width: height; height: 65px;">
-          </td>
-        </tr>
-      </table>
+  <div style="width: 487.5pt; background-image: url(''); background-repeat: no-repeat; background-size: cover; opacity: 1; position: absolute;">
+    <img class="bgCover" src="/images/logohead.png" alt="">
+    <table style="border-collapse: collapse; margin-left: 70pt;" cellspacing="0">
+      <tr style="height: 50pt; width: 200pt;">
+        <td style="width: 70pt; border-style: solid;">
+          <img src="/images/logohead.png" alt="" style="width: height; height: 65px;">
+        </td>
+        <td style="width: 120%; font-weight: bold; font-family: Arial, Helvetica, sans-serif; text-align: center; border-style: solid;">
+          RAPOR PROJEK<br> PENGUATAN PROFIL PELAJAR PANCASILA
+        </td>
+        <td style="width: 60pt; text-align: end; border-style: solid;">
+          <img src="/images/stiker.png" alt="" style="width: height; height: 65px;">
+        </td>
+      </tr>
+    </table>
 
-      <br><br>
+    <br><br>
 
-      <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
-        <tr>
-          <td style="width: 100pt;">
-            Nama Sekolah
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 200pt;">
-            SMP Muhammadiyah 1 Malang
-          </td>
-          <td style="width: 40pt;">
-            Kelas
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 80pt;">
-            VII (Tujuh)
-          </td>
-        </tr>
-        <tr>
-          <td style="width: 100pt;">
-            Alamat Sekolah
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 200pt;">
-            Jl. Brigjen Slamet Riadi 134 Malang
-          </td>
-          <td style="width: 40pt;">
-            Fase
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 80pt;">
-            D
-          </td>
-        </tr>
-        <tr>
-          <td style="width: 100pt;">
-            Nama Siswa
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 200pt;">
-            Devy Yuliana Putri
-          </td>
-          <td style="width: 40pt;">
-            TA
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 80pt;">
-            2022/2023
-          </td>
-        </tr>
-        <tr>
-          <td style="width: 100pt;">
-            NISN
-          </td>
-          <td style="width: 6pt;">
-            :
-          </td>
-          <td style="width: 200pt;">
-            0102230527
-          </td>
-          <td style="width: 40pt;">
-          </td>
-          <td style="width: 6pt;">
-          </td>
-          <td style="width: 80pt;">
-          </td>
-        </tr>
-      </table>
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="width: 100pt;">
+          Nama Sekolah
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          SMP Muhammadiyah 1 Malang
+        </td>
+        <td style="width: 40pt;">
+          Kelas
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          VII (Tujuh)
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          Alamat Sekolah
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          Jl. Brigjen Slamet Riadi 134 Malang
+        </td>
+        <td style="width: 40pt;">
+          Fase
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          D
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          Nama Siswa
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          Devy Yuliana Putri
+        </td>
+        <td style="width: 40pt;">
+          TA
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          2022/2023
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          NISN
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          0102230527
+        </td>
+        <td style="width: 40pt;">
+        </td>
+        <td style="width: 6pt;">
+        </td>
+        <td style="width: 80pt;">
+        </td>
+      </tr>
+    </table>
 
-      <br>
+    <br>
 
-      <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
-        <tr>
-          <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
-            PROYEK 1: Ajang Kompetensi dan Kreativitas Siswa
-          </td>
-        </tr>
-        <tr>
-          <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
-          </td>
-        </tr>
-      </table>
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 1: Ajang Kompetensi dan Kreativitas Siswa
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table>
 
-      <br>
+    <br>
 
-      <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
-        <tr>
-          <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
-            PROYEK 2: Serba Serbi Malangku
-          </td>
-        </tr>
-        <tr>
-          <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
-          </td>
-        </tr>
-      </table>
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 2: Serba Serbi Malangku
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table>
 
-      <br>
+    <br>
 
-      <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
-        <tr>
-          <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
-            PROYEK 3: Sampahku Masa Depanku
-          </td>
-        </tr>
-        <tr>
-          <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
-          </td>
-        </tr>
-      </table>
-    </div>
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 3: Sampahku Masa Depanku
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table>
+
+    <br><br>
+<!-- 
+    <table style="border-collapse: collapse; margin-left: 70pt" cellspacing="0">
+      <tr style="height: 50pt;">
+        <td style="width: 70pt;">
+          <img src="/images/logohead.png" alt="" style="width: height; height: 65px;">
+        </td>
+        <td style="font-weight: bold; font-family: Arial, Helvetica, sans-serif; text-align: center;">
+          RAPOR PROJEK<br> PENGUATAN PROFIL PELAJAR PANCASILA
+        </td>
+        <td style="width: 65pt; text-align: end;">
+          <img src="/images/stiker.png" alt="" style="width: height; height: 65px;">
+        </td>
+      </tr>
+    </table>
+
+    <br><br>
+
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="width: 100pt;">
+          Nama Sekolah
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          SMP Muhammadiyah 1 Malang
+        </td>
+        <td style="width: 40pt;">
+          Kelas
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          VII (Tujuh)
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          Alamat Sekolah
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          Jl. Brigjen Slamet Riadi 134 Malang
+        </td>
+        <td style="width: 40pt;">
+          Fase
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          D
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          Nama Siswa
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          Devy Yuliana Putri
+        </td>
+        <td style="width: 40pt;">
+          TA
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 80pt;">
+          2022/2023
+        </td>
+      </tr>
+      <tr>
+        <td style="width: 100pt;">
+          NISN
+        </td>
+        <td style="width: 6pt;">
+          :
+        </td>
+        <td style="width: 200pt;">
+          0102230527
+        </td>
+        <td style="width: 40pt;">
+        </td>
+        <td style="width: 6pt;">
+        </td>
+        <td style="width: 80pt;">
+        </td>
+      </tr>
+    </table>
+
+    <br>
+
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 1: Ajang Kompetensi dan Kreativitas Siswa
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table>
+
+    <br>
+
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 2: Serba Serbi Malangku
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table>
+
+    <br>
+
+    <table style="border-collapse: collapse; margin-left: 40pt;" cellspacing="0">
+      <tr>
+        <td style="height: 25pt; width: 375pt; padding-left: 30pt; font-weight: bold;">
+          PROYEK 3: Sampahku Masa Depanku
+        </td>
+      </tr>
+      <tr>
+        <td style="height: 110pt; width: 50pt; text-align: justify; border-style: solid;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu cursus vitae congue mauris rhoncus aenean. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Volutpat odio facilisis mauris sit amet massa vitae. Quam vulputate dignissim suspendisse in est ante. Odio pellentesque diam volutpat commodo sed. Sed enim ut sem viverra aliquet eget.
+        </td>
+      </tr>
+    </table> -->
   </div>
 </body>
 
